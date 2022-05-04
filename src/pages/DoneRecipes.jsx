02e4ac03ccc2371291499da2224b750e2cbf9a6a
SerjoFrancisco <spinheiro.lf@gmail.com>
2022-05-04 16:33:44 -0300

    tela  de receitas prontas, pronta

--- src/pages/DoneRecipes.jsx
+++ src/pages/DoneRecipes.jsx
@@ -6,39 +6,25 @@ import CardDoneRecipeFood from '../components/CardDoneRecipeFood';
 function DoneRecipes() {
   const [recipesDone, setRecipesDone] = useState([]);
 
-  const doneRecipes = [
-    {
-      id: '52771',
-      type: 'food',
-      nationality: 'Italian',
-      category: 'Vegetarian',
-      alcoholicOrNot: '',
-      name: 'Spicy Arrabiata Penne',
-      image: 'https://www.themealdb.com/images/media/meals/ustsqw1468250014.jpg',
-      doneDate: '23/06/2020',
-      tags: ['Pasta', 'Curry'],
-    },
-    {
-      id: '178319',
-      type: 'drink',
-      nationality: '',
-      category: 'Cocktail',
-      alcoholicOrNot: 'Alcoholic',
-      name: 'Aquamarine',
-      image: 'https://www.thecocktaildb.com/images/media/drink/zvsre31572902738.jpg',
-      doneDate: '23/06/2020',
-      tags: [],
-    },
-  ];
+  const filterFood = () => {
+    const recipes = localStorage.getItem('doneRecipes');
+    const foods = JSON.parse(recipes).filter((recipe) => recipe.type === 'food');
+    setRecipesDone(foods);
+  };
 
-  const handleFood = () => {
-    localStorage.setItem('doneRecipes', JSON.stringify(doneRecipes));
+  const filterDrink = () => {
+    const recipes = localStorage.getItem('doneRecipes');
+    const drinks = JSON.parse(recipes).filter((recipe) => recipe.type === 'drink');
+    setRecipesDone(drinks);
+  };
+
+  const handleDone = () => {
     const recipes = localStorage.getItem('doneRecipes');
     setRecipesDone(JSON.parse(recipes));
   };
 
   useEffect(() => {
-    handleFood();
+    handleDone();
   // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
@@ -49,18 +35,21 @@ function DoneRecipes() {
         <button
           data-testid="filter-by-all-btn"
           type="button"
+          onClick={ handleDone }
         >
           All
         </button>
         <button
           data-testid="filter-by-food-btn"
           type="button"
+          onClick={ filterFood }
         >
           Foods
         </button>
         <button
           data-testid="filter-by-drink-btn"
           type="button"
+          onClick={ filterDrink }
         >
           Drinks
         </button>
