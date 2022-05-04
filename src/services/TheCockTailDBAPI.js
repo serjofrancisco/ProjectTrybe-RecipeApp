e05bfc38156126dd60a989e703ff97fbfb30d8a7
Sergio Francisco <93877790+SerjoFrancisco@users.noreply.github.com>
2022-05-04 20:17:54 -0300

    Revert "[Main-group-23] - Tela de explorar ingredientes"

--- src/services/TheCockTailDBAPI.js
+++ src/services/TheCockTailDBAPI.js
@@ -1,5 +1,4 @@
 const SURPRISEDRINK = 'https://www.thecocktaildb.com/api/json/v1/1/random.php';
-const LISTINGREDIENTS = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list';
 
 export const getDrinksCategory = async () => {
   const response = await fetch('https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list');
@@ -25,12 +24,6 @@ export const filterDrinkByCategory = async (category) => {
 
 export const getSurpriseDrink = async () => {
   const response = await fetch(SURPRISEDRINK);
-  const json = await response.json();
-  return response.ok ? Promise.resolve(json) : Promise.reject(json);
-};
-
-export const getIngredientsDrinks = async () => {
-  const response = await fetch(LISTINGREDIENTS);
   const json = await response.json();
   return response.ok ? Promise.resolve(json) : Promise.reject(json);
 };
